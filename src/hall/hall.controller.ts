f5f6f982f8de6ec198eb11b4cc2cb5d28b2c0c68
JetGreen <grishadzyin@gmail.com>
2024-06-22 09:36:39 +0400

    create hall method

--- src/hall/hall.controller.ts
+++ src/hall/hall.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from '@nestjs/common';
+// core imports
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, Req, UseGuards } from '@nestjs/common';
+
+// interfaces
+import { HallFromClient } from './interfaces/hall-from-client.interface';
+
+// services
+import { HallService } from './hall.service';
 
 @Controller('hall')
 export class HallController {
-  
+  constructor(
+    private HallService: HallService,
+  ) { }
+  @Post()
+  async create(@Body('hall') hall: HallFromClient) {
+    return await this.HallService.create(hall)
+  }
 }
---
+++ src/hall/interfaces/hall-from-client.interface.ts
@@ -0,0 +1,3 @@
+export interface HallFromClient {
+  title: string,
+}
