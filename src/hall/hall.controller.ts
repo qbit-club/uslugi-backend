4402f1c5837999de2e728562f552fad56c5487a7
JetGreen <grishadzyin@gmail.com>
2024-06-23 16:23:56 +0400

    get all halls

--- src/hall/hall.controller.ts
+++ src/hall/hall.controller.ts
@@ -7,13 +7,24 @@ import { HallFromClient } from './interfaces/hall-from-client.interface';
 // services
 import { HallService } from './hall.service';
 
+// all about MongoDB
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { HallClass } from './schemas/hall.schema';
+
 @Controller('hall')
 export class HallController {
   constructor(
     private HallService: HallService,
+    @InjectModel('Hall') private HallModel: Model<HallClass>,
   ) { }
   @Post()
   async create(@Body('hall') hall: HallFromClient) {
-    return await this.HallService.create(hall)
+    return await this.HallModel.create(hall)
+  }
+
+  @Get('/all')
+  async getAll() {
+    return await this.HallModel.find({})
   }
 }
--- src/hall/hall.service.ts
+++ src/hall/hall.service.ts
@@ -1,18 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
-// all about MongoDB
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { HallClass } from './schemas/hall.schema';
-
-// interfaces
-import { HallFromClient } from './interfaces/hall-from-client.interface';
 @Injectable()
 export class HallService {
   constructor(
-    @InjectModel('Hall') private HallModel: Model<HallClass>,
   ) {}
-  create(hall: HallFromClient): Promise<any> {    
-    return this.HallModel.create(hall)
-  }
 }
