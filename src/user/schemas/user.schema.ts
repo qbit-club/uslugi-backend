88f4ae3bcb5ed49c9670966071ec61156013b383
JetGreen <grishadzyin@gmail.com>
2024-07-08 17:08:10 +0400

    add rests array to user

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,35 +1,41 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<UserClass>
 
 @Schema()
 export class UserClass {
-  @Prop({ 
-    type: String, 
+  @Prop({
+    type: String,
     required: true,
     min: 2
   })
   name: string
 
-  @Prop({ 
-    type: String, 
+  @Prop({
+    type: String,
     required: true,
   })
   email: string
 
-  @Prop({ 
-    type: String, 
+  @Prop({
+    type: String,
     required: true,
   })
   password: string
 
   @Prop({
-    type: [String], 
+    type: [String],
     default: [],
     required: false
   })
   roles: string[]
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Rest' }],
+    default: [],
+  })
+  rests: mongoose.Schema.Types.ObjectId[]
 }
 
 export const UserSchema = SchemaFactory.createForClass(UserClass)
