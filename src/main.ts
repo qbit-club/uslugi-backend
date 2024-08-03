74aa475ae24530166221b4010ff919a1c5e6a2ab
qbclub <17926057+qbclub@users.noreply.github.com>
2024-08-03 12:09:11 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
-  app.enableCors({ 
+  // Настройка CORS
+  const corsOptions: CorsOptions = {
     origin: [process.env.CLIENT_URL, 'http://localhost:3001', 'https://glazovest.ru'],
-    credentials: true
-  })
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true, // Разрешение на отправку cookie и авторизационных заголовков
+  };
+  app.enableCors(corsOptions);
   app.useGlobalFilters(new HttpExceptionFilter())
 
   app.use(cookieParser())
