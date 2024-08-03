acd9fe7fbaa2b4864e0ee46460506560c67f365c
qbclub <17926057+qbclub@users.noreply.github.com>
2024-08-03 13:02:51 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
-  // Настройка CORS
-  const corsOptions: CorsOptions = {
-    origin: [process.env.CLIENT_URL, 'http://localhost:3001', 'https://glazovest.ru'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true, // Разрешение на отправку cookie и авторизационных заголовков
-  };
-  app.enableCors(corsOptions);
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors({ 
+    origin: [process.env.CLIENT_URL, 'http://localhost:3001','https://glazovest.ru'],
+    credentials: true
+  })
   app.useGlobalFilters(new HttpExceptionFilter())
 
   app.use(cookieParser())
