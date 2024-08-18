bf0ae29abc8330074cb9a0f8277fd4905f8bd85e
JetGreen <grishadzyin@gmail.com>
2024-08-18 15:20:21 +0400

    forgot cors

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  // app.enableCors({ 
-  //   origin: [process.env.CLIENT_URL, 'http://localhost:3001', 'https://glazovest.ru'],
-  //   credentials: true
-  // })
+  app.enableCors({ 
+    origin: [process.env.CLIENT_URL, 'http://localhost:3001', 'https://glazovest.ru'],
+    credentials: true
+  })
   app.useGlobalFilters(new HttpExceptionFilter())
 
   app.use(cookieParser())
